b812f83061549c754fc2315a799ea75e64563982
Igor <scr58e@gmail.com>
2019-05-27 02:30:26 +0300

    test 1

--- src/player.ts
+++ src/player.ts
@@ -1,10 +1,13 @@
 export default class Player {
+
     playerY = 0;
     playerX = 0;
     playerName = '';
+
     setPlayerName(name:string){
         this.playerName = name;
     };
+
     constructor(x:number, y:number){
         this.playerY = y;
         this.playerX = x;
